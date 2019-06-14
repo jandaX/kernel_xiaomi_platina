9ac9e8064370aa547a9fffee500c38417b8d41ab
Tyler Nijmeh <tylernij@gmail.com>
2019-06-13 18:00:13 -0700

    msm: mdss: Disable CABC by default
    The CABC commands seem to dull the display colors. Additionally, other
    users find the automatic contrast control distracting. Disable the
    setting by default.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    # Conflicts:
    #	drivers/video/fbdev/msm/mdss_dsi_panel.c
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -73,6 +73,14 @@ void mdss_dsi_ulps_suspend_enable(bool enable)
 		mdss_pinfo->ulps_suspend_enabled = enable;
 }
 
+static bool ce_enable = true;
+static bool srgb_enable = true;
+static bool cabc_enable = false;
+
+module_param(ce_enable, bool, 0644);
+module_param(srgb_enable, bool, 0644);
+module_param(cabc_enable, bool, 0644);
+
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	if (ctrl->pwm_pmi)
